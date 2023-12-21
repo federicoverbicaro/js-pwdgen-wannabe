db6336080b026ef2e4472f3b731afc4efcb5f8b8
federicoverbicaro <federico.verbicaro@icloud.com>
2023-12-21 15:24:07 +0100

    feat: Termine Esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+// Chiedi i dati all'utente
+
+// nome persona
+let nomePersona = prompt("Inserisci il tuo nome:");
+
+// cognome persona
+let cognomePersona = prompt("Inserisci il tuo cognome:");
+
+// colore preferito
+let colorePreferito = prompt("Inserisci il tuo colore preferito:");
+
+// password
+const password = nomePersona + cognomePersona + colorePreferito + "23"
+
+
+document.writeln(password);
+
+
+
+
+
+
+
+
+
+
